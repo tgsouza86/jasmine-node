e3989bf2b6c4ff4fe2e8211df1407ec3f57d8758
tgsouza86 <50930848+tgsouza86@users.noreply.github.com>
2019-06-17 11:54:42 -0300

    Arquivos do projeto local

---
+++ spec/exemplo/exemplo.Spec.js
@@ -0,0 +1,15 @@
+describe("Suite de teste de exemplo com NodeJS", function(){
+
+
+	it("deve demonstrar o uso do Jasmine com o 	NodeJS", function() {
+
+		expect(true).toEqual(true);
+	});
+
+	it("deve demonstar o uso do Karma em modo de escuta", function() {
+		expect(1).toEqual(1)
+	});
+
+
+	
+});
